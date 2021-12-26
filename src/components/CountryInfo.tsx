de30d8b271f992e742236de33c7e96a4c7de6b37
borshon420 <borshonarafat420@gmail.com>
2021-12-27 01:29:10 +0600

    data fetching

--- src/components/CountryInfo.tsx
+++ src/components/CountryInfo.tsx
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+interface InitProps {
+    name: string;
+}
+
+interface InitCountry {
+    capital: string[];
+    population: number;
+    latlng: number[];
+    flags: {
+        svg: string;
+    }
+}
 
 const CountryInfo: React.FC = () => {
+    const name = useParams<InitProps>();
+
+    useEffect(()=> {
+        getCountry();
+    }, [])
+
+    const getCountry = async () => {
+        try {
+            const res = await fetch(`https://restcountries.com/v3.1/name/${name}`)
+            const data = await res.json();
+            console.log(data)
+        }catch(error){
+            console.log(error);
+        }
+    }
     return (
         <div>
             <h2>This is country Info</h2>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,42 +1,41 @@
-import React, {useState} from 'react';
-import TextField from '@mui/material/TextField';
-import { Button } from '@mui/material';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import { useHistory } from "react-router-dom";
 
 const Home: React.FC = () => {
+  const history = useHistory();
+  const [countryName, setCountryName] = useState<string>("");
 
-    const history = useHistory();
-    const [countryName, setCountryName] = useState<string>("");
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCountryName(event.target.value);
+  };
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setCountryName(event.target.value);
-    }
-
-    const onSubmit = () => {
-        history.push(`/country/${countryName}`)
-    }
-    return (
-        <div>
-            <h2>Welcome To Weather Application</h2>
-            <div>
-            <TextField 
-            variant="standard"
-            placeholder="Enter Country"
-            value={countryName}
-            onChange={handleInputChange}
-            />
-            </div>
-            <Button 
-            size="medium"
-            variant="contained"
-            disabled={!countryName}
-            onClick={onSubmit}
-            style={{width: "210px", marginTop: "20px" }}
-            >
-                Submit
-            </Button>
-        </div>
-    );
+  const onSubmit = () => {
+    history.push(`/country/${countryName}`);
+  };
+  return (
+    <div>
+      <h2>Welcome To Weather Application</h2>
+      <div>
+        <TextField
+          variant="standard"
+          placeholder="Enter Country"
+          value={countryName}
+          onChange={handleInputChange}
+        />
+      </div>
+      <Button
+        size="medium"
+        variant="contained"
+        disabled={!countryName}
+        onClick={onSubmit}
+        style={{ width: "210px", marginTop: "20px" }}
+      >
+        Submit
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
